bfeab0fe6039185adac0e4ffa8b9fb5ca0c43965
MoChips <31714669+smedhan1@users.noreply.github.com>
2018-10-13 23:50:10 -0500

    update #3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // random number generated from 19-120
 $(document).ready(function () {
-    
+
     // random number to be shown at start of game from 19-120
     var randomNumber = Math.floor(Math.random() * 102) + 19;
 
@@ -9,37 +9,55 @@ $(document).ready(function () {
     var num2 = Math.floor(Math.random() * 12) + 1;
     var num3 = Math.floor(Math.random() * 12) + 1;
     var num4 = Math.floor(Math.random() * 12) + 1;
-    
+
+
     //storing score and total values here
     var totalScore = 0;
     var winCount = 0;
     var loseCount = 0;
-    
+
     //appends the random number to html id.
     $("#RNG").text(randomNumber);
 
     // create a click function for each gem
     // assign random number to each click function
+    // link gem number to the user score
     $("#blue-gem").on("click", function () {
         totalScore += num1;
         $("#score").text(totalScore);
+        game()
     });
     $("#red-gem").on("click", function () {
         totalScore += num2;
         $("#score").text(totalScore);
+        game()
     });
     $("#green-gem").on("click", function () {
         totalScore += num3;
         $("#score").text(totalScore);
+        game()
     });
     $("#yellow-gem").on("click", function () {
         totalScore += num4;
         $("#score").text(totalScore);
+        game()
     });
 
-    
-    // link gem number to the user score
-    // when gem number is equal to 
+
+    // when user score is equal to gem total, you win
+    function game() {
+        if (totalScore === randomNumber) {
+            winCount++;
+            $("#Wins").text(winCount);
+            console.log(winCount);
+        } else (totalScore > randomNumber) {
+            loseCount++;
+            $("#Losses").text(loseCount);
+        }
+    }
+    // when user score is greater than gem number, you lose
     // tally wins and loses
+
+
 
 });
